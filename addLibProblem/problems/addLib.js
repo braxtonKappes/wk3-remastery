6e5237f529b8b87b692e80563937baded2106d98
Braxton Kappes <braxtonkappes@gmail.com>
2022-12-12 13:06:57 -0600

    added problems

---
+++ addLibProblem/problems/addLib.js
@@ -0,0 +1,5 @@
+// your code here!
+
+
+console.log(makeAddLib("swim", "sparkly", "rainbow")); // => "I shall swim to the sparkly rainbow?"
+console.log(makeAddLib("RUN", "FANCY", "ParK")); // => "I shall RUN to the FANCY ParK?"
